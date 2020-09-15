7ad63758f3ae7d5829746d1f73489a9e0e51cf09
rbrito <ra.brito@yahoocom.br>
2020-09-15 16:12:05 -0300

    adicionada flake8 como dependencia
    close #12

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # libpythonpro
 Modulo para exemplificar projetos de Python do curso Pytools
+
+Nesse curso é ensinado como contrubuir para projetos com codigo aberto
+
+Suporta Python 3
+
+Para instalar:
+
+```console
+python3 -m venv .venv
+source .venv/bin/activate
+pip isntall -r requiriments-dev.txt
+```
+Para conferir qualidade do codigo:
+
+```console
+flake8
+```
--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def buscar_avatar (usuario):
+def buscar_avatar(usuario):
     """
     busca avatar de um usuario no github
     :param usuario: str com nome do usuario no github
@@ -10,7 +10,7 @@ def buscar_avatar (usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+
 if __name__ == '__main__':
     print(buscar_avatar('raolbrito'))
-    
-
