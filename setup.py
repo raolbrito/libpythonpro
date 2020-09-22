7d95ccec1609674ee8c59c9d36b81bf2b9ba04d1
rbrito <ra.brito@yahoocom.br>
2020-09-22 12:40:22 -0300

    alterando 3 setup.py

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro_raolbrito"
+PACKAGE = "libpythonpro"
 NAME = 'raol_libpyhtonpro'
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Rafael Brito"
